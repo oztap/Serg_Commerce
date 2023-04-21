b143f4672065f3b6fdada0b479d24a993fee9379
Sergii Ostapchuk <sergiiostapchuk1@gmail.com>
2023-04-20 21:36:58 -0500

    End of section two

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _repo;
+
+        public ProductsController(IProductRepository repo)
+        {
+            _repo = repo;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _repo.GetProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _repo.GetProductByIdAsync(id);
+        }
+
+    }
+}
