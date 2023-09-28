9b1d75c4934e53807a068b36da5213125f49961a
Sergii Ostapchuk <sergiiostapchuk1@gmail.com>
2023-09-27 21:26:05 -0500

    End of section 4

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -3,11 +3,10 @@ using Core.Entities;
 namespace Core.Interfaces
 {
     public interface IProductRepository
-     {
+    {
         Task<Product> GetProductByIdAsync(int id);
-        
         Task<IReadOnlyList<Product>> GetProductsAsync();
         Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
         Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
-    } 
+    }
 }
