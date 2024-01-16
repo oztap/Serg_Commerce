6c2c55962b7278057bf1e2514e96513eebbe7ed1
Sergii Ostapchuk <sergiiostapchuk1@gmail.com>
2024-01-16 14:54:38 -0600

    End of section 14

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'SkiNet';
 
-  constructor(){}
+  constructor(private basketService: BasketService) {}
 
-  ngOnInit(): void {}
-      
-    }
+  ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if(basketId) this.basketService.getBasket(basketId);
+  }
+}
